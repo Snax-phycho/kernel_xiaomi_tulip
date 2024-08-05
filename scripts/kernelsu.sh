c5aa0c8cb273ca24504e2a806d199582ce69e3ef
GhostMaster69-dev <rathore6375@gmail.com>
2024-08-05 11:18:26 +0000

    scripts: Bump KernelSU to v1.0.1
    Signed-off-by: GhostMaster69-dev <rathore6375@gmail.com>

--- scripts/kernelsu.sh
+++ scripts/kernelsu.sh
@@ -53,10 +53,11 @@ fi
 if [ $(git -C $KERNELSU describe --abbrev=0 --tags) = "v0.9.4" ]; then
     git -C $KERNELSU cherry-pick -n --no-gpg-sign 7af4f338e56cce386ba0dccdc0871b687cd697b1
 fi
-if [ $(git -C $KERNELSU describe --abbrev=0 --tags) = "v1.0.0" ]; then
+for release in "v1.0.0" "v1.0.1"; do
+if [ $(git -C $KERNELSU describe --abbrev=0 --tags) = $release ]; then
     git -C $KERNELSU revert -n --no-gpg-sign 898e9d4f8ca9b2f46b0c6b36b80a872b5b88d899
 fi
 if [ $(git -C $KERNELSU describe --abbrev=0 --tags) = "v1.0.0" ]; then
     git -C $KERNELSU cherry-pick -n --no-gpg-sign 78bf2493bf2b92a12636a4ec64ded21e33644441
 fi
-
+done
